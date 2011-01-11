ab2cab150e20832aaf7cc26494acfc6b7b1d0f0c
Tim Wingfield <tim@timwingfield.com>
2011-01-11 16:43:22 -0500

    Adding code samples for codemash talk

---
+++ Codemash.2011/samples/features/step_definitions/falcon_steps.rb
@@ -0,0 +1,28 @@
+Given /^the Falcon is flying$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^the destination is "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^Han says "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the Falcon goes to lightspeed$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^the flight origin is "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the Falcon does not go to lightspeed$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the message value is "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
